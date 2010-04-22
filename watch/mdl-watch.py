01f2cf3923640cce702a3630aa357612863df473
unknown <izlesa@.(none)>
2010-04-22 05:28:11 +0400

    add functionality in script
    write link from rss to file in this format
    date|link|description

--- watch/mdl-watch.py
+++ watch/mdl-watch.py
@@ -1,12 +1,36 @@
 import feedparser
 import re
+import time
 
+def normalize_description(string):
+	found = re.search(ur"Description\s*:\s*(.+)", string).group(1)
+	return found
+
+
+filename = r".\mURL.txt"
 mdl = feedparser.parse("http://www.malwaredomainlist.com/hostslist/mdl.xml")
 
-for entry in mdl.entries:
-	match = re.search(ur"Host: ([\S.]+),", entry.description)
-	if(match == False):
-	    match = re.search(ur"IP address: ([\S.]+),", entry.description)
+match = re.compile(ur"^Host: (\S+),")
+ipmatch = re.compile(ur"IP\s+address:\s+(\S+),")
 
-	print match.group(1)
+fn = file(filename, "w")
+
+
+for entry in mdl.entries:
+	print entry.description
+	found = match.search(entry.description).group(1)
+	if found == False or found == '-':
+		found = ipmatch.search(entry.description).group(1)
+		if found == False:
+			found = "None"
+
+
+	now = time.localtime()
+	fn.write("%s.%s.%s|" % (now[2], now[1], now[0]))
+	fn.write(found+'|')
+	fn.write(normalize_description(entry.description)+'\n')
+
+fn.close()
+
+
 
